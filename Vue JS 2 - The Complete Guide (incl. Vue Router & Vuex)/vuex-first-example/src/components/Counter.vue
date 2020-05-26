466b16b4e8a69abbd7c811b0eecefc5e83db99f0
Oussama-Tn <Oussama.BelGhaieb@gmail.com>
2020-05-26 20:36:22 +0200

    :rocket: Vuex first example

---
+++ Vue JS 2 - The Complete Guide (incl. Vue Router & Vuex)/vuex-first-example/src/components/Counter.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <button class="btn btn-primary" @click="increment">Increment</button>
+        <button class="btn btn-primary ml-10" @click="decrement">Decrement</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            increment() {
+                this.$store.state.counter++;
+            },
+            decrement() {
+                this.$store.state.counter--;
+            }
+        }
+    }
+</script>
+<style scoped>
+  .ml-10 {
+    margin-left: 10px;
+  }
+</style>
